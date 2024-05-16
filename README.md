e6f16eff6e618ba5bddefd9992b813baf738dd9f
Yong Park <pyongho0306@gmail.com>
2024-05-15 18:17:16 -0700

    Added CORS to handle different ports

---
+++ README.md
@@ -0,0 +1,1 @@
+Used Cors to handle the port 3000(Frontend) and 5001(Backend)
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const loanRoutes = require("./routes/loanRoutes");
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
-
 app.use("/api", loanRoutes);
 
 const PORT = process.env.PORT || 5000;
