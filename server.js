5380b5a949cccaa6ac6fc692131ba18e0ba0d2d5
Yong Park <pyongho0306@gmail.com>
2024-05-15 18:17:50 -0700

    Fixed port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", loanRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
